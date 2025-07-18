7bc2f42186d7ce71a7926102c18d9aea29372e69
foxthebarefoot <foxthebarefoot@gmail.com>
2025-07-19 00:18:11 +0900

    change css style of webzine lead

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://gidda.kr', // ✨ 당신의 도메인 URL을 반드시 입력하세요.
-    integrations: [sitemap()],
+  // ✨ 당신의 도메인 URL을 반드시 입력하세요.
+  site: 'https://gidda.kr',
+
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
